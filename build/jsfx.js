4a3fb2c066a8973bd9830112b4ec7c81e516bafc
inssein <hussein@jafferjee.ca>
2015-07-29 21:27:24 -0700

    rebuild

--- build/jsfx.js
+++ build/jsfx.js
@@ -158,16 +158,16 @@ var __extends = (this && this.__extends) || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 };
-/**
- * @filter           Brightness / Contrast
- * @description      Provides additive brightness and multiplicative contrast control.
- * @param brightness -1 to 1 (-1 is solid black, 0 is no change, and 1 is solid white)
- * @param contrast   -1 to 1 (-1 is solid gray, 0 is no change, and 1 is maximum contrast)
- */
 var jsfx;
 (function (jsfx) {
     var filter;
     (function (filter) {
+        /**
+         * @filter           Brightness / Contrast
+         * @description      Provides additive brightness and multiplicative contrast control.
+         * @param brightness -1 to 1 (-1 is solid black, 0 is no change, and 1 is solid white)
+         * @param contrast   -1 to 1 (-1 is solid gray, 0 is no change, and 1 is maximum contrast)
+         */
         var BrightnessContrast = (function (_super) {
             __extends(BrightnessContrast, _super);
             function BrightnessContrast(brightness, contrast) {
@@ -212,6 +212,135 @@ var jsfx;
         })(jsfx.Filter);
         filter.BrightnessContrast = BrightnessContrast;
     })(filter = jsfx.filter || (jsfx.filter = {}));
+})(jsfx || (jsfx = {}));
+var jsfx;
+(function (jsfx) {
+    var filter;
+    (function (filter) {
+        /**
+         * @filter           Hue / Saturation
+         * @description      Provides rotational hue and multiplicative saturation control. RGB color space
+         *                   can be imagined as a cube where the axes are the red, green, and blue color
+         *                   values. Hue changing works by rotating the color vector around the grayscale
+         *                   line, which is the straight line from black (0, 0, 0) to white (1, 1, 1).
+         *                   Saturation is implemented by scaling all color channel values either toward
+         *                   or away from the average color channel value.
+         * @param hue        -1 to 1 (-1 is 180 degree rotation in the negative direction, 0 is no change,
+         *                   and 1 is 180 degree rotation in the positive direction)
+         * @param saturation -1 to 1 (-1 is solid gray, 0 is no change, and 1 is maximum contrast)
+         */
+        var HueSaturation = (function (_super) {
+            __extends(HueSaturation, _super);
+            function HueSaturation(hue, saturation) {
+                _super.call(this, null, "\n            uniform sampler2D texture;\n            uniform float hue;\n            uniform float saturation;\n            varying vec2 texCoord;\n\n            void main() {\n                vec4 color = texture2D(texture, texCoord);\n\n                /* hue adjustment, wolfram alpha: RotationTransform[angle, {1, 1, 1}][{x, y, z}] */\n                float angle = hue * 3.14159265;\n                float s = sin(angle), c = cos(angle);\n                vec3 weights = (vec3(2.0 * c, -sqrt(3.0) * s - c, sqrt(3.0) * s - c) + 1.0) / 3.0;\n                color.rgb = vec3(\n                    dot(color.rgb, weights.xyz),\n                    dot(color.rgb, weights.zxy),\n                    dot(color.rgb, weights.yzx)\n                );\n\n                /* saturation adjustment */\n                float average = (color.r + color.g + color.b) / 3.0;\n                if (saturation > 0.0) {\n                    color.rgb += (average - color.rgb) * (1.0 - 1.0 / (1.001 - saturation));\n                } else {\n                    color.rgb += (average - color.rgb) * (-saturation);\n                }\n\n                gl_FragColor = color;\n            }\n        ");
+                // set properties
+                this.properties.hue = jsfx.Filter.clamp(-1, hue, 1) || 0;
+                this.properties.saturation = jsfx.Filter.clamp(-1, saturation, 1) || 0;
+            }
+            HueSaturation.prototype.drawCanvas = function (imageData) {
+                var pixels = imageData.data;
+                var hue = this.properties.hue;
+                var saturation = this.properties.saturation;
+                var angle = hue * 3.14159265;
+                var sin = Math.sin(angle);
+                var cos = Math.cos(angle);
+                var average, r, g, b, rgb, weights;
+                for (var i = 0; i < pixels.length; i += 4) {
+                    if (hue) {
+                        rgb = new jsfx.util.Vector3(pixels[i] / 255, pixels[i + 1] / 255, pixels[i + 2] / 255);
+                        // apply hue
+                        weights = new jsfx.util.Vector3(2 * cos, -Math.sqrt(3.0) * sin - cos, Math.sqrt(3.0) * sin - cos);
+                        // add 1 and average the vector
+                        weights
+                            .addScalar(1.0)
+                            .divideScalar(3.0);
+                        // set rgb
+                        r = rgb.dot(weights);
+                        g = rgb.dotScalars(weights.z, weights.x, weights.y);
+                        b = rgb.dotScalars(weights.y, weights.z, weights.x);
+                    }
+                    else {
+                        r = pixels[i] / 255;
+                        g = pixels[i + 1] / 255;
+                        b = pixels[i + 2] / 255;
+                    }
+                    // apply saturation
+                    average = (r + g + b) / 3;
+                    if (saturation > 0) {
+                        r += (average - r) * (1 - 1 / (1.001 - saturation));
+                        g += (average - g) * (1 - 1 / (1.001 - saturation));
+                        b += (average - b) * (1 - 1 / (1.001 - saturation));
+                    }
+                    else {
+                        r += (average - r) * (-saturation);
+                        g += (average - g) * (-saturation);
+                        b += (average - b) * (-saturation);
+                    }
+                    // set values
+                    pixels[i] = r * 255;
+                    pixels[i + 1] = g * 255;
+                    pixels[i + 2] = b * 255;
+                }
+                return imageData;
+            };
+            return HueSaturation;
+        })(jsfx.Filter);
+        filter.HueSaturation = HueSaturation;
+    })(filter = jsfx.filter || (jsfx.filter = {}));
+})(jsfx || (jsfx = {}));
+/**
+ * Vector3 Utility Class
+ *  -> Taken from https://github.com/mrdoob/three.js/blob/master/src/math/Vector3.js with only the functions we need.
+ */
+var jsfx;
+(function (jsfx) {
+    var util;
+    (function (util) {
+        var Vector3 = (function () {
+            function Vector3(x, y, z) {
+                this.x = x;
+                this.y = y;
+                this.z = z;
+            }
+            Vector3.prototype.addScalar = function (s) {
+                this.x += s;
+                this.y += s;
+                this.z += s;
+                return this;
+            };
+            Vector3.prototype.multiplyScalar = function (s) {
+                this.x *= s;
+                this.y *= s;
+                this.z *= s;
+                return this;
+            };
+            Vector3.prototype.divideScalar = function (s) {
+                if (s !== 0) {
+                    var invScalar = 1 / s;
+                    this.x *= invScalar;
+                    this.y *= invScalar;
+                    this.z *= invScalar;
+                }
+                else {
+                    this.x = 0;
+                    this.y = 0;
+                    this.z = 0;
+                }
+                return this;
+            };
+            Vector3.prototype.length = function () {
+                return Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z);
+            };
+            Vector3.prototype.dot = function (v) {
+                return this.x * v.x + this.y * v.y + this.z * v.z;
+            };
+            Vector3.prototype.dotScalars = function (x, y, z) {
+                return this.x * x + this.y * y + this.z * z;
+            };
+            return Vector3;
+        })();
+        util.Vector3 = Vector3;
+    })(util = jsfx.util || (jsfx.util = {}));
 })(jsfx || (jsfx = {}));
 var jsfx;
 (function (jsfx) {
