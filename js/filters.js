4c931f227bb4c76cdf47d0970eb6869b54b1aa18
inssein <hussein@jafferjee.ca>
2015-08-02 19:02:21 -0700

    demo for new effects

--- js/filters.js
+++ js/filters.js
@@ -23,6 +23,22 @@
       function(renderer) {
         renderer.applyFilter(new jsfx.filter.HueSaturation(this.hue, this.saturation));
       }
-    )
+    ),
+    new demo.Filter('Sepia', function() {
+      this.addSlider('amount', 'Amount', 0, 1, 1, 0.01);
+    }, function(renderer) {
+      renderer.applyFilter(new jsfx.filter.Sepia(this.amount));
+    }),
+    new demo.Filter('Blur', function() {
+      this.addSlider('radius', 'Radius', 0, 180, 20, 1);
+    }, function(renderer) {
+      renderer.applyFilter(new jsfx.filter.Blur(this.radius));
+    }),
+    new demo.Filter('Unsharp Mask', function() {
+      this.addSlider('radius', 'Radius', 0, 180, 20, 1);
+      this.addSlider('strength', 'Strength', 0, 5, 2, 0.01);
+    }, function(renderer) {
+      renderer.applyFilter(new jsfx.filter.UnsharpMask(this.radius, this.strength));
+    })
   ];
 })();
